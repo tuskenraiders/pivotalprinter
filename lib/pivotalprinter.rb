5381600070797265ea5c4847db5dfc5d79fccd07
Florian Frank <flori@ping.de>
2013-01-07 10:35:41 +0100

    move more code and images into lib dir

--- lib/pivotalprinter.rb
+++ lib/pivotalprinter.rb
@@ -1,15 +1,19 @@
 module Pivotalprinter
+  class ConfigNeeded < StandardError; end
+  class TokenMissing < StandardError; end
+  class ProjectMissing < StandardError; end
 end
 
 require 'pivotalprinter/version'
-require "pivotalprinter/client"
-require "pivotalprinter/iteration"
-require "pivotalprinter/story"
+require 'pivotalprinter/client'
+require 'pivotalprinter/iteration'
+require 'pivotalprinter/story'
+require 'pivotalprinter/printer'
 require 'term/ansicolor'
 require 'prawn'
 require 'prawn/layout/grid'
 require 'tins/xt/full'
 require 'trollop'
 require 'yaml'
-require "open-uri"
-require "nokogiri"
+require 'open-uri'
+require 'nokogiri'
